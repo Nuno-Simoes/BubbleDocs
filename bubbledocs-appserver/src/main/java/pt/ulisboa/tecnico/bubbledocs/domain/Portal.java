6bc062c9c1130ef082bde36052bd2e89855077e8
Pedro <Pedro@194.210.223.25>
2015-03-11 16:47:14 +0000

    hf

--- bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/domain/Portal.java
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/domain/Portal.java
@@ -20,6 +20,18 @@ public class Portal extends Portal_Base {
 	private Portal() {
 		FenixFramework.getDomainRoot().setPortal(this);
 	}
+	
+	public List<Spreadsheet> listSpreadsheets (String username) {
+    	List<Spreadsheet> list = new ArrayList<Spreadsheet>();
+    	
+    	for (User u : this.getUsersSet()) {
+    		if (u.getUsername().equals(username)) {
+    			u.listSpreadsheets(list);
+    		}
+    	} 
+    	
+    	return list;
+    }
 
 	public List<Spreadsheet> listSpreadsheets (User user, String str) {
     	List<Spreadsheet> list = new ArrayList<Spreadsheet>();
@@ -36,6 +48,14 @@ public class Portal extends Portal_Base {
     public void removeSpreadsheet (User u) {
     	for (Spreadsheet s : this.getSpreadsheetsSet()) {
     		if (s.getOwner().equals(u.getUsername())) {
+    			this.removeSpreadsheets(s);
+    		}
+    	}
+    }
+    
+    public void removeSpreadsheet (String username, String sheetName) {
+    	for (Spreadsheet s : this.getSpreadsheetsSet()) {
+    		if (s.getOwner().equals(username) && s.getName().equals(sheetName)) {
     			this.removeSpreadsheets(s);
     		}
     	}
@@ -69,6 +89,14 @@ public class Portal extends Portal_Base {
     		return true;
     	}
     	return false;
+    }
+    
+    public void listUsers () {
+    	for (User u : this.getUsersSet()) {
+    		System.out.print("Username: " + u.getUsername());
+    		System.out.print(" Name: " + u.getName());
+    		System.out.println(" Password: " + u.getPassword());
+    	}
     }
     
 }
