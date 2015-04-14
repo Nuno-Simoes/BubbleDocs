1c37f3d5978c2756c2ed489b001263b57c4e8eda
Nuno-Simoes <nunosimoes_86@hotmail.com>
2015-04-14 15:21:52 +0100

    Fixed AssingLiteralToCellServiceTest Fixed AssingReferenceToCellServiceTest Fixed CreateSpreadsheetServiceTest

--- bubbledocs-appserver/src/test/java/pt/ulisboa/tecnico/bubbledocs/service/AssignReferenceToCellServiceTest.java
+++ bubbledocs-appserver/src/test/java/pt/ulisboa/tecnico/bubbledocs/service/AssignReferenceToCellServiceTest.java
@@ -15,33 +15,42 @@ import pt.ulisboa.tecnico.bubbledocs.exceptions.SpreadsheetDoesNotExistException
 public class AssignReferenceToCellServiceTest extends BubbleDocsServiceTest {
 
     // the tokens
-    private String ars;
-    private String smf;
-    private int id;
+	private String logars;
+	private String logsam;
+	private int id;
 
-    private static final String USERNAME = "ars";
-    private static final String NO_PERMISSION_USER = "smf";
-    private static final String PASSWORD = "ars";
-    private static final String NAME = "teste";
+	private static final String USERNAME = "ars";
+	private static final String NAME = "António Rito Silva";
+	private static final String EMAIL = "ars@ars.com";
+	private static final String NO_PERMISSION_USERNAME = "sam";
+	private static final String NO_PERMISSION_NAME = "Sam";
+	private static final String NO_PERMISSION_EMAIL = "sam@sam.com";
+	private static final String PASSWORD = "randomm";
+    private static final String SPREADNAME = "teste";
     private static final int ID_DOES_NOT_EXIST = 100;
     
     @Override
     public void populate4Test() {
-        User u = createUser(USERNAME, PASSWORD, "António Rito Silva");
-        createUser(NO_PERMISSION_USER, PASSWORD, "Smf");
-        Spreadsheet s = createSpreadSheet(u, NAME, 10, 10);
+    	User ars = createUser(USERNAME, NAME, EMAIL);
+        ars.setPassword(PASSWORD);
+        
+        User sam = createUser(NO_PERMISSION_USERNAME, NO_PERMISSION_NAME, 
+        		NO_PERMISSION_EMAIL);
+        sam.setPassword(PASSWORD);
+        
+        Spreadsheet s = createSpreadSheet(ars, SPREADNAME, 10, 10);
         s.getCell(7, 3).setIsProtected(true);
         id = s.getId();
-        ars = addUserToSession("ars");
-        smf = addUserToSession("smf");
+        logars = addUserToSession(USERNAME);
+        logsam = addUserToSession(NO_PERMISSION_USERNAME);
     }
 
     @Test
     public void success() {
-    	AssignReferenceCellService service = new AssignReferenceCellService(ars,
+    	AssignReferenceCellService service = new AssignReferenceCellService(logars,
     			id, "1;1","3;3");
         service.execute();
-        Spreadsheet sheet = super.getSpreadSheet(NAME);
+        Spreadsheet sheet = super.getSpreadSheet(SPREADNAME);
         Reference ref = (Reference) sheet.getCell(1, 1).getContent();
         assertEquals(3, ref.getReferencedCell().getLine(), 0);
         assertEquals(3, ref.getReferencedCell().getColumn(), 0);
@@ -49,42 +58,42 @@ public class AssignReferenceToCellServiceTest extends BubbleDocsServiceTest {
 
     @Test(expected = SpreadsheetDoesNotExistException.class)
     public void spreadsheetDoesNotExist() {
-    	AssignReferenceCellService service = new AssignReferenceCellService(ars,
+    	AssignReferenceCellService service = new AssignReferenceCellService(logars,
     			ID_DOES_NOT_EXIST, "1;1","3;3");
         service.execute();
     }
 
     @Test(expected = InvalidPermissionException.class)
     public void invalidUser() {
-    	AssignReferenceCellService service = new AssignReferenceCellService(smf,
+    	AssignReferenceCellService service = new AssignReferenceCellService(logsam,
     			id, "1;1","3;3");
         service.execute();
     }
 
     @Test(expected = OutOfBoundsException.class)
     public void outOfBounds() {
-    	AssignReferenceCellService service = new AssignReferenceCellService(ars,
+    	AssignReferenceCellService service = new AssignReferenceCellService(logars,
     			id, "30;5","3;2");
         service.execute();
     }
     
     @Test(expected = OutOfBoundsException.class)
     public void outOfBounds2() {
-    	AssignReferenceCellService service = new AssignReferenceCellService(ars,
+    	AssignReferenceCellService service = new AssignReferenceCellService(logars,
     			id, "3;2","30;5");
         service.execute();
     }
 
     @Test(expected = InvalidContentException.class)
     public void notAReference() {
-    	AssignReferenceCellService service = new AssignReferenceCellService(ars,
+    	AssignReferenceCellService service = new AssignReferenceCellService(logars,
     			id, "1;1","failed");
         service.execute();
     }
     
     @Test(expected = InvalidPermissionException.class)
     public void protectedCell() {
-    	AssignReferenceCellService service = new AssignReferenceCellService(ars,
+    	AssignReferenceCellService service = new AssignReferenceCellService(logars,
     			id, "7;3","3;7");
         service.execute();
     }
