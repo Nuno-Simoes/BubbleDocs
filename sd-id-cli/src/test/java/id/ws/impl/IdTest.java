b842046c5f8aa4f78e201aff33724cb910b710fc
AfrAndrade <andre.f.andrade@iol.pt>
2015-04-17 16:42:38 +0100

    Client Implementation.

---
+++ sd-id-cli/src/test/java/id/ws/impl/IdTest.java
@@ -0,0 +1,46 @@
+package id.ws.impl;
+
+import java.util.Map;
+
+import javax.xml.registry.JAXRException;
+import javax.xml.ws.BindingProvider;
+
+import org.junit.*;
+
+import pt.ulisboa.tecnico.sdis.Id.ws.SDId;
+import pt.ulisboa.tecnico.sdis.Id.ws.SDId_Service;
+import Id.ws.uddi.UDDINaming;
+import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
+
+public class Id {
+
+	static private String uddiURL = "http://localhost:8081";
+	static private String name = "SdId";
+	static protected SDId port;
+		    	
+    //static members
+    //one-time initialisation and clean-up
+    @BeforeClass
+    public static void oneTimeSetUp() throws JAXRException {
+    	UDDINaming uddiNaming = new UDDINaming(uddiURL);
+    	String endpointAddress = uddiNaming.lookup(name);
+    	SDId_Service service = new SDId_Service();
+    	port = service.getSDIdImplPort();
+    	BindingProvider bindingProvider = (BindingProvider) port;
+        Map<String, Object> requestContext = bindingProvider.getRequestContext();
+        requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
+    }
+
+    @AfterClass
+    public static void oneTimeTearDown() {
+    	port = null;
+    }
+
+    //members
+    //initialisation and clean-up for each test
+    @Before
+    public void setUp() {}
+
+    @After
+    public void tearDown() {}
+}
