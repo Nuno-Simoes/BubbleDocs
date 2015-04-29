32f29ba49b6ff1bef099b92ff4b751bf7e9d7c7c
ritap420 <ritap420@gmail.com>
2015-04-29 15:14:54 +0100

    AssignLiteralToCell closes #65 and closes #80

--- bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/integration/AssignLiteralToCellIntegrator.java
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/integration/AssignLiteralToCellIntegrator.java
@@ -1,5 +1,27 @@
 package pt.ulisboa.tecnico.bubbledocs.integration;
 
-public class AssignLiteralToCellIntegrator {
+import pt.ulisboa.tecnico.bubbledocs.service.AssignLiteralToCellService;
+
+public class AssignLiteralToCellIntegrator extends BubbleDocsIntegrator {
+
+	private String accessUsername;
+	private int docId;
+	private String cellId;
+	private String literal;
+
+	public AssignLiteralToCellIntegrator(String accessUsername, int docId,
+			String cellId, String literal) {
+		this.accessUsername = accessUsername;
+		this.docId = docId;
+		this.cellId = cellId;
+		this.literal = literal;
+	}
+	
+	@Override
+	protected void dispatch() throws Exception {
+		AssignLiteralToCellService localService = 
+				new AssignLiteralToCellService(accessUsername, docId, cellId, literal);
+		localService.execute();
+	}
 
 }
